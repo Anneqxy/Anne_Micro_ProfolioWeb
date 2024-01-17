a0116aa6848fd055dcb1bfe8d426638585604cb2
Anneqxy <113318686+Anneqxy@users.noreply.github.com>
2024-01-17 11:38:08 -0600

    Update App.jsx at siteProps

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Anne Que",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  email: "xque2@illinois.edu",
+  gitHub: "Anneqxy",
+  instagram: "",
+  linkedIn: "https://www.linkedin.com/in/xiangyue-que-251178258/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
