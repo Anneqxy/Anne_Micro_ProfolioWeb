5bfef5d606e925a02db09b83e00d34bab46ad387
Anneqxy <113318686+Anneqxy@users.noreply.github.com>
2024-01-17 17:53:14 -0600

    Update Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/anne.jpeg";
+import image from "../images/Anne.jpeg";
 
 const imageAltText = "It is me. I traveled to Qingdao in China.";
 
