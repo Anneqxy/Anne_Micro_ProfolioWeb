e8c6a9f9231a3db03105dccb275806a6fc41720f
Anneqxy <113318686+Anneqxy@users.noreply.github.com>
2024-01-17 12:16:42 -0600

    Update Portfolio.jsx
    change the image to my picture

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/anne.jpeg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "It is me. I traveled to Qingdao in China.";
 
 /**
  * Project list
